4054d2d7be75942be40e29341a8c944de73934a0
Wilgot <wilgotwardell@hotmail.com>
2022-10-11 18:24:35 +0200

    -

--- src/App.js
+++ src/App.js
@@ -10,9 +10,10 @@ function App() {
   const [movies, setMovies] = useState([]);
   const [imageConfig, setImageConfig] = useState([]);
 
-  async function searchForMovies (searchTerm) {
+  async function searchForMovies (searchTerm, filter) {
     const movieInformation = {
       type: "movies",
+      filter: filter,
       searchTerm: searchTerm
     }
     const response = await FetchMovies(movieInformation);
@@ -30,14 +31,15 @@ function App() {
     fetchImageConfig()
   }, [])
   
+  
 
   return (
     <div className="App">
       <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
-      <button onClick={() => searchForMovies(searchTerm)}>Search</button>
+      <button onClick={() => searchForMovies(searchTerm, "H")}>Search</button>
       <div className='center'>
         <div className='discover-grids'>
-          {movies.length > 0 ? movies[0].results.map(movie => <Movie movie={movie} imageConfig={imageConfig[0].images}/>) : <p>Search For Movies!</p>}      
+          {movies.length > 0 ? movies[0].map(movie => <Movie key={movie.id} movie={movie} imageConfig={imageConfig[0].images}/>) : <p>Search For Movies!</p>}      
         </div>
       </div>
     </div>
--- src/Components/FetchMovies.jsx
+++ src/Components/FetchMovies.jsx
@@ -6,7 +6,16 @@ export default async function FetchMovies(movieInformation) {
         case "movies": {
             const respone = await fetch("https://api.themoviedb.org/3/search/movie?api_key=d3967aacd6e3ef518c42a75acde3d777&language=en-US&query=" + movieInformation.searchTerm +"&page=1&include_adult=false")
                 .then(res=>res.clone().json());
-            return respone;
+            
+            const movies = respone.results;
+            console.log(movies);
+            if(movieInformation.filter == ""){
+                return movies;
+            }else{
+                // const filteredMovies = movies.filter(movie.)
+                
+                return movies;
+            }
         }
         case "config": {
             const respone = await fetch("https://api.themoviedb.org/3/configuration?api_key=d3967aacd6e3ef518c42a75acde3d777")
