e993743abcdbe22b196b3dde1a1129d58df0a62a
Wilgot Wardell <wilgotwardell@hotmail.com>
2022-10-12 15:47:57 +0200

    -

--- src/Components/Movie.jsx
+++ src/Components/Movie.jsx
@@ -1,20 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import GenerateImage from './GenerateImage'
 
 export default function Movie({movie, imageConfig}) {
   const moviePoster = imageConfig.base_url + imageConfig.poster_sizes[2] + movie.poster_path;
+  const [isHovering, setIsHovering] = useState(false);
   return (
-    <div style={{width: "fit-content"}}>
-      <div style={{position:"absolute", display: "flex", alignItems: "center", height: "100%"}}>
-
-        {/* <div style={{color: "white"}}>{movie.title}</div> */}
-      </div>
+    <div style={{width: "fit-content"}} onMouseEnter={() => setIsHovering(true)} onMouseLeave={() => setIsHovering(false)}>
+      {/* <div style={{position:"absolute", display: "flex", alignItems: "center", height: "100%"}}>
+        
+      </div> */}
       {/* <p>popularity: {movie.popularity}</p>
       <p>release date: {movie.release_date}</p> */}
-      <img 
-        src={moviePoster}
-        className="movie-poster"
-      ></img>
+      <div style={{
+        position: "relative",
+        textAlign: "center",
+        color: "white"
+        }}>
+        <img 
+          src={moviePoster}
+          className="movie-poster"
+        ></img>
+        {isHovering &&
+          <div style={{
+            userSelect: "none",
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+          }}>{movie.title}</div>
+        }
+      </div>
     </div>
   )
 }
