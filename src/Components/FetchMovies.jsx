8c73978275d076aedae7091dcc5fc6f8f3f83fb3
Wilgot Wardell <wilgotwardell@hotmail.com>
2022-10-12 13:16:34 +0200

    -

--- src/Components/FetchMovies.jsx
+++ src/Components/FetchMovies.jsx
@@ -4,15 +4,14 @@ const key = "d3967aacd6e3ef518c42a75acde3d777";
 export default async function FetchMovies(movieInformation) {
     switch(movieInformation.type) {
         case "movies": {
-            const respone = await fetch("https://api.themoviedb.org/3/search/movie?api_key=d3967aacd6e3ef518c42a75acde3d777&language=en-US&query=" + movieInformation.searchTerm +"&page=1&include_adult=false")
+            const respone = await fetch("https://api.themoviedb.org/3/search/movie?api_key="+key+"&language=en-US&query=" + movieInformation.searchTerm +"&page=1&include_adult=false")
                 .then(res=>res.clone().json());
             
             const movies = respone.results;
-            console.log(movies);
             if(movieInformation.filter == ""){
                 return movies;
             }else{
-                // const filteredMovies = movies.filter(movie.)
+                const filteredMovies = movies.filter(movie => movie)
                 
                 return movies;
             }
@@ -21,6 +20,7 @@ export default async function FetchMovies(movieInformation) {
             const respone = await fetch("https://api.themoviedb.org/3/configuration?api_key=d3967aacd6e3ef518c42a75acde3d777")
                 .then(res=>res.clone().json());
             return respone;
+            const response = await fetch("https://api.themoviedb.org/3/genre/movie/list?api_key=d3967aacd6e3ef518c42a75acde3d777&language=en-US")
         }
         default: {
             alert("Error");
