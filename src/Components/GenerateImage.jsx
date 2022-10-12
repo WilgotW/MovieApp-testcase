0b2db0b3e552f4c7bd5edd3ad376d662fbc584c7
Wilgot <wilgotwardell@hotmail.com>
2022-10-12 19:02:35 +0200

    -

--- src/Components/GenerateImage.jsx
+++ src/Components/GenerateImage.jsx
@@ -1,6 +1,3 @@
-import React from 'react'
-
-export default function GenerateImage({baseUrl, size, path}) {
-    console.log(size)
-    return (baseUrl + size + path);
+export default function GenerateImage(baseUrl, size, path) {
+    return baseUrl + size + path;
 }
--- src/Components/Movie.jsx
+++ src/Components/Movie.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import GenerateImage from './GenerateImage'
 
 export default function Movie({movie, imageConfig}) {
-  const moviePoster = imageConfig.base_url + imageConfig.poster_sizes[2] + movie.poster_path;
+  const moviePoster = GenerateImage(imageConfig.base_url, imageConfig.poster_sizes[2], movie.poster_path);
   const [isHovering, setIsHovering] = useState(false);
   return (
     <div style={{width: "fit-content"}} onMouseEnter={() => setIsHovering(true)} onMouseLeave={() => setIsHovering(false)}>
@@ -16,13 +16,16 @@ export default function Movie({movie, imageConfig}) {
           className="movie-poster"
         ></img>
         {isHovering &&
-          <div style={{
+          <section style={{
+            pointerEvents: "none",
+            fontWeight: "600",
+            width: "100%",
             userSelect: "none",
             position: "absolute",
             top: "50%",
             left: "50%",
-            transform: "translate(-50%, -50%)",
-          }}>{movie.title}</div>
+            transform: "translate(-50%, -50%)"
+          }}>{movie.title} <br/> <p style={{fontWeight: "200", margin: "0"}}> {movie.release_date}</p> </section>
         }
       </div>
     </div>
