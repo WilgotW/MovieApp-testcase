65e11e0a48b3a2fab226eb1b9ae22627fdc1bfbd
Wilgot <wilgotwardell@hotmail.com>
2022-10-13 22:05:08 +0200

    -

--- src/Components/SwitchPage.jsx
+++ src/Components/SwitchPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function ShowMoreButton({page, name}) {
+export default function SwitchPage({page, name}) {
   return (
     <div>
         <button onClick={page}>{name}</button>
