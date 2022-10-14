13cc092e821da5d8117a73d7f8aec498514ed44b
Wilgot Wardell <wilgotwardell@hotmail.com>
2022-10-14 13:43:05 +0200

    -

--- src/Components/GetApiResponse.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function GetApiResponse(movieInformation) {
-    let response;
-    async function getMovies(){
-        response = await fetch("https://api.themoviedb.org/3/search/movie?api_key="+key+"&language=en-US&query=" + movieInformation.searchTerm +"&page="+movieInformation.page+"&include_adult=false")
-    }
-    return response.json();
-}
