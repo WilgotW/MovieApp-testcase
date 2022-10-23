afa51a345ba4b8bd799ec03f552b5b6488bf186d
Wilgot <wilgotwardell@hotmail.com>
2022-10-23 21:30:29 +0200

    -

--- src/ApiCalls/getFilteredMovies.js
+++ src/ApiCalls/getFilteredMovies.js
@@ -1,13 +1,11 @@
 export default async function getFilteredMovies(genreName, genreIds, page, minRating) {
     const id = genreIds.genres.map(genre => {  
         if(genre.name == genreName){
-            console.log(genre.name + " has the id: " + genre.id)
             return genre.id;
         }
     });
     const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=${id}&with_watch_monetization_types=flatrate`)
     const movies = await response.json();
     const ratedFilter = movies.results.filter(movie => movie.vote_average >= minRating)
-    console.log(await movies)
     return await ratedFilter;
 }
--- src/ApiCalls/getImagesConfig.js
+++ src/ApiCalls/getImagesConfig.js
@@ -1,6 +1,5 @@
 export default async function getImagesConfig() {
     const response = await fetch(`https://api.themoviedb.org/3/configuration?api_key=${process.env.REACT_APP_API_KEY}`)
     const config = await response.json();
-    console.log(await config);
     return await config.images;
 }
